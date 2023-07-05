f1960dc20f5266a3bf4cb0a7a53b145d46fbbaba
tienbui <tienbui1508@gmail.com>
2023-07-05 20:47:50 +0930

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Tien Bui on 5/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
