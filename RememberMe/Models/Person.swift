3c8df0fb69eb7f7cd60052545f3deac6aeb16155
tienbui <tienbui1508@gmail.com>
2023-07-07 03:13:33 +0930

    Change sort by last name to sort by first name

--- RememberMe/Models/Person.swift
+++ RememberMe/Models/Person.swift
@@ -24,10 +24,10 @@ struct Person: Identifiable, Codable, Comparable {
     static let example = Person(id: UUID(), firstName: "Luffy", lastName: "Monkey D.", description: "Monkey D. Luffy, also known as \"Straw Hat Luffy\" and commonly as \"Straw Hat\", is the founder and captain of the increasingly infamous and powerful Straw Hat Pirates, as well as the most powerful of its top fighters. He desires to find the legendary treasure left behind by the late Gol D. Roger and thereby become the Pirate King, which would help facilitate an unknown dream of his that he has told only to Shanks, his brothers, and crew. He believes that being the Pirate King means having the most freedom in the world.")
     
     static func < (lhs: Person, rhs: Person) -> Bool {
-        if lhs.lastName == rhs.lastName {
-            return lhs.firstName.lowercased() < rhs.firstName.lowercased()
+        if lhs.firstName == rhs.firstName {
+            return lhs.lastName.lowercased() < rhs.lastName.lowercased()
         }
-        return lhs.lastName.lowercased() < rhs.lastName.lowercased()
+        return lhs.firstName.lowercased() < rhs.firstName.lowercased()
     }
 
 }
